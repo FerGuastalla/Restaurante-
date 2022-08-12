2f956c1432c58f0e6e548473fceaaaed11cd0cdd
FerGuastalla <111152338+FerGuastalla@users.noreply.github.com>
2022-08-12 19:01:19 -0300

    Create Dados.php

---
+++ Trabalho/Dados.php
@@ -0,0 +1,12 @@
+<?php
+    $marmitas = array(
+        0 => array('id'=>'1', 'nome'=>'Carne Assada com Pure', 'tamanho'=>'400g','preco'=>'27,30','ingredientes'=>'Pure de batata, Carne ou molho madeira com Shitake','imagem'=>'Imagem/marmita1.jpg'),
+        1 => array('id'=>'2', 'nome'=>'Frango ao Gergilim', 'tamanho'=>'360g','preco'=>'25,90','ingredientes'=>'File de frango, Arroz branco com gergilim, e Mix de legumes','imagem'=>'Imagem/marmita2.png'),
+        2 => array('id'=>'3', 'nome'=>'Bobó de Camarão', 'tamanho'=>'400g','preco'=>'30,90','ingredientes'=>'Bobó de camarão, Arroz branco e Brócolis','imagem'=>'Imagem/marmita3.jpg'),
+        3 => array('id'=>'4', 'nome'=>'Strogonoff de Boi', 'tamanho'=>'350g','preco'=>'26,90','ingredientes'=>'Strogonoff de filé mignon, Arroz branco e Batata frita','imagem'=>'Imagem/marmita4.jpg'),
+        4 => array('id'=>'5', 'nome'=>'Strogonoff de Frango', 'tamanho'=>'350g','preco'=>'24,90','ingredientes'=>'Strogonoff de frango, Arroz branco e Batata palha','imagem'=>'Imagem/marmita5.jpg'),
+        5 => array('id'=>'6', 'nome'=>'Parmeggiana de Frango', 'tamanho'=>'415g','preco'=>'25,60','ingredientes'=>'Filé de frango a parmeggiana, Arroz branco, Batata friga e Salada de maionese','imagem'=>'Imagem/marmita6.png'),
+        6 => array('id'=>'7', 'nome'=>'Fitness', 'tamanho'=>'350g','preco'=>'25,70','ingredientes'=>'Ingredientes: Creme de mandioquinha, Carne moida com cenoura e Brócolis','imagem'=>'Imagem/marmita7.jpg'),
+        7 => array('id'=>'8', 'nome'=>'Feijoada', 'tamanho'=>'500g','preco'=>'33,50','ingredientes'=>'Feijoada, Arroz branco, Couve picadinha e Torresmo','imagem'=>'Imagem/marmita8.jpg'),
+    );
+    ?>
